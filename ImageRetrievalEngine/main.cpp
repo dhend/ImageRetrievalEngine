3e689344ff8eb0bdbe595cdfcf3c8ae09909c9c2
Rui Xu <xurui011@gmail.com>
2015-02-27 11:56:08 +0800

    Initial Commit

---
+++ ImageRetrievalEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageRetrievalEngine
+//
+//  Created by XuRui on 15-2-27.
+//  Copyright (c) 2015年 XuRui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
