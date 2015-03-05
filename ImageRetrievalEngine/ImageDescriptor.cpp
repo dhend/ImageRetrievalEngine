5c601cafed9360c02bd1965f23fefa70aa6b6963
Rui Xu <xurui011@gmail.com>
2015-03-05 22:52:12 +0800

    Update ImageDescriptor.cpp
    Update ImageDescriptor.cpp

--- ImageRetrievalEngine/ImageDescriptor.cpp
+++ ImageRetrievalEngine/ImageDescriptor.cpp
@@ -10,23 +10,23 @@
 
 void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::vector<double>& correlogram) {
     cv::Mat img_HSV;
-    const int distance = 10;
-    const int color_num = 30;
+    const int distance = 5;
+    const int color_num = 90;
     std::vector<int> pix_num(color_num, 0);
     std::vector<cv::Mat> splited;
     cv::cvtColor(img, img_HSV, CV_BGR2HSV);
     cv::split(img_HSV, splited);
     cv::Mat img_H = splited[0];
     cv::Mat img_S = splited[1];
-
+    
     for (int i = 0; i < img.rows; ++i) {
         uchar *data_H = img_H.ptr<uchar>(i);
-		uchar *data_S = img_S.ptr<uchar>(i);
+        uchar *data_S = img_S.ptr<uchar>(i);
         for (int j = 0; j < img.cols; ++j) {
-            int color_H = (int)data_H[j] / 6;
-			int color_S = (int)data_S[j] / 9;
+            int color_H = (int)data_H[j] / 2;
+            int color_S = (int)data_S[j] / 3;
             pix_num[color_H]++;
-            for (int d = 1; d <= distance; ++d) {
+            for (int d = 1; d <= 2 * distance; d = d + 2) {
                 int samecolor_num = 0;
                 int total_num = 0;
                 //distance = max{|x1 - x2|, |y1 - y2|}
@@ -38,10 +38,10 @@ void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::v
                         continue;
                     }
                     uchar color_H_uchar = img_H.at<uchar>(up_row, col);
-		    uchar color_S_uchar = img_S.at<uchar>(up_row, col);
-		    int color_H_d = ((int) color_H_uchar) / 6;
-		    int color_S_d = ((int) color_S_uchar) / 9;
-                    if (color_H == color_H_d && color_S == color_S_d) {
+                    uchar color_S_uchar = img_S.at<uchar>(up_row, col);
+                    int color_H_d = ((int) color_H_uchar) / 2;
+                    int color_S_d = ((int) color_S_uchar) / 3;
+                    if (abs(color_H - color_H_d) < 3 && abs(color_S - color_S_d) < 3) {
                         samecolor_num++;
                     }
                     total_num++;
@@ -54,10 +54,10 @@ void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::v
                         continue;
                     }
                     uchar color_H_uchar = img_H.at<uchar>(down_row, col);
-		    uchar color_S_uchar = img_S.at<uchar>(down_row, col);
-		    int color_H_d = ((int) color_H_uchar) / 6;
-		    int color_S_d = ((int) color_S_uchar) / 9;
-                    if (color_H == color_H_d && color_S == color_S_d) {
+                    uchar color_S_uchar = img_S.at<uchar>(down_row, col);
+                    int color_H_d = ((int) color_H_uchar) / 2;
+                    int color_S_d = ((int) color_S_uchar) / 3;
+                    if (abs(color_H - color_H_d) < 3 && abs(color_S - color_S_d) < 3) {
                         samecolor_num++;
                     }
                     total_num++;
@@ -70,10 +70,10 @@ void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::v
                         continue;
                     }
                     uchar color_H_uchar = img_H.at<uchar>(row, left_col);
-		    uchar color_S_uchar = img_S.at<uchar>(row, left_col);
-		    int color_H_d = ((int) color_H_uchar) / 6;
-		    int color_S_d = ((int) color_S_uchar) / 9;
-                    if (color_H == color_H_d && color_S == color_S_d) {
+                    uchar color_S_uchar = img_S.at<uchar>(row, left_col);
+                    int color_H_d = ((int) color_H_uchar) / 2;
+                    int color_S_d = ((int) color_S_uchar) / 3;
+                    if (abs(color_H - color_H_d) < 3 && abs(color_S - color_S_d) < 3) {
                         samecolor_num++;
                     }
                     total_num++;
@@ -86,15 +86,15 @@ void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::v
                         continue;
                     }
                     uchar color_H_uchar = img_H.at<uchar>(row, right_col);
-		    uchar color_S_uchar = img_S.at<uchar>(row, right_col);
-		    int color_H_d = ((int) color_H_uchar) / 6;
-		    int color_S_d = ((int) color_S_uchar) / 9;
-                    if (color_H == color_H_d && color_S == color_S_d) {
+                    uchar color_S_uchar = img_S.at<uchar>(row, right_col);
+                    int color_H_d = ((int) color_H_uchar) / 2;
+                    int color_S_d = ((int) color_S_uchar) / 3;
+                    if (abs(color_H - color_H_d) < 3&& abs(color_S - color_S_d) < 3) {
                         samecolor_num++;
                     }
                     total_num++;
                 }
-                correlogram[(d - 1) * color_num + color_H] += (double)samecolor_num / (double)total_num;
+                correlogram[(d / 2 - 1) * color_num + color_H] += (double)samecolor_num / (double)total_num;
             }
         }
     }
@@ -108,8 +108,8 @@ void ColorAutoCorrelogram::CreateColorAutoCorrelogram(const cv::Mat& img, std::v
 }
 
 double ColorAutoCorrelogram::CorrelogramMatching(const std::vector<double>& correlogram1, const std::vector<double>& correlogram2) {
-    const int distance = 10;
-    const int color_num = 30;
+    const int distance = 5;
+    const int color_num = 90;
     double confidenceValue = 0;
     for(int i = 0; i < distance; i++) {
         for(int j = 0; j < color_num; j++) {
